c12cdc87cdcd61da13d9d82e3bc9a559e649a210
unknown <evgeny.naranovich@gmail.com>
2022-02-27 21:56:48 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './scss/app.scss'
+const elem1 = document.getElementById('elem1')
+function func1 () {
+  alert(this.value)
+}
+func1.call(elem1)
 
-console.log('Hello')
+const elem2 = document.getElementById('elem2')
+
+function func2 (surname, name) {
+  alert(this.value + ', ' + surname + ' ' + name)
+}
+func2.apply(elem2, ['иван', 'иван'])
+
+function func3 (surname, name) {
+  alert(this.value + ', ' + surname + ' ' + name)
+}
+
+func3.bind(elem2)('Иванов', 'Иван')
+func3.bind(elem2)('Петров', 'Петр')
